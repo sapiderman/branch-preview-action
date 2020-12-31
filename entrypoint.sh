6ae38863a42221b647130da5e177ed846855aa07
Budhi <sapiderman.lumpat@gmail.com>
2020-12-31 12:41:30 +0700

    ci: update shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Starting action"
 
 eval "$(ssh-agent -s)"
 
-echo "Setting up SSH..."
+echo "Setting up SSH"
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 echo -e "$DOKKU_KEY" > ~/.ssh/id_rsa
@@ -13,31 +13,31 @@ ssh-keyscan -p "$PORT" "$HOST" >> ~/.ssh/known_hosts
 
 cd "$GITHUB_WORKSPACE" || exit
 
-DEFAULT_BRANCH=${DEFAULT_BRANCH:-$GITHUB_MAIN_BRANCH}
+DEFAULT_BRANCH=${DEFAULT_BRANCH:-$GITHUB_PRIMARY_BRANCH}
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-if [[ "$CURRENT_BRANCH" == "$DEFAULT_BRANCH" && -n "$DOMAIN" ]]; then
-  APP_NAME=$DOMAIN
+if [[ $CURRENT_BRANCH == "$DEFAULT_BRANCH" && -n $DOMAIN ]]; then
+    echo "using domain as app_name: $DOMAIN"
+    APP_NAME=$DOMAIN
 else
-  APP_NAME=${CURRENT_BRANCH/\//-}
+    echo "using current_branch as app_name: ${CURRENT_BRANCH/\//-}"
+    APP_NAME=${CURRENT_BRANCH/\//-}
 fi
-
 echo "APP_NAME defined as $APP_NAME"
 
 echo "Checking if app exists"
-ssh "dokku@$HOST" -p "$PORT" dokku apps:exists "\$APP_NAME"
-RSLT="$?"
-
-if [[ "$RSLT" != 0 ]]; then
-  echo "The app does not exist yet, creating the app: $APP_NAME"
-  ssh "dokku@$HOST" -p "$PORT" dokku apps:create "\$APP_NAME"
+if ! ssh "dokku@$HOST" -p "$PORT" 'apps:exists $APP_NAME'; then
+    echo "The app does not exist yet, creating the app: $APP_NAME"
+    ssh "dokku@$HOST" -p "$PORT" 'apps:create $APP_NAME'
 fi
 
 echo "Deploying to host: $HOST"
 git fetch --unshallow
-git remote add "$APP_NAME" "dokku@$HOST:$PORT/$APP_NAME"
+git remote add "$APP_NAME" "ssh://dokku@$HOST:$PORT/$APP_NAME"
+
 echo "pushing changes to app:$APP_NAME"
-git push -f "$APP_NAME" "$CURRENT_BRANCH:master"
-echo "done... thank you."
-
-
+if ! git push -f "$APP_NAME" "$CURRENT_BRANCH:master"; then
+    echo "something went wrong..."
+else
+    echo "done... thank you"
+fi
